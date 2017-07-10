186ffbdd8cf06ca725e32e806053f5d5c8256985
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-10 13:50:45 -0500

    Now using Opacity to .animate() each box on and off

--- public/js/simple-simon.js
+++ public/js/simple-simon.js
@@ -1,3 +1,5 @@
+"use strict";
+
 //1 Make button funtion
 //2 display a pattern
 //3 check if user pattern matches display
@@ -10,32 +12,26 @@ var displayArray = [];
 var userArray = [];
 
 $('#controls').click(function(){
-	var random = Math.floor((Math.random() * 4) + 1);
-	var brighten = '';
-	switch (random) {
-		case 1:
-			brighten = 'blue';
-			break;
-		case 2:
-			brighten = 'red';
-			break;
-		case 3:
-			brighten = 'green';
-			break;
-		case 4:
-			brighten = 'yellow';
-			break;
-	}
-	$('#box' + random).css('background', brighten);
-
+	lightUpSquare();
 })
+
 
 $('.boxes').click(function(){
-	return userArray += this.id
+	return userArray.push(this.id);
 
 })
 
+function lightUpSquare(){
+	var random = Math.floor((Math.random() * 4) + 1);
 
+	//Using jQuery to brighten and darken colors via opacity
+	$('#box' + random).animate({
+		opacity: 1
+	}, 500).animate({
+		opacity: .4
+	}, 500)
+
+}
 
 
 
