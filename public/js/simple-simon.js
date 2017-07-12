89f150f8d9abe3092ada3a68d6fc32eb64dfa21d
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-12 16:00:09 -0500

    Completed Project for now, may add more when PHP is added

--- public/js/simple-simon.js
+++ public/js/simple-simon.js
@@ -8,10 +8,14 @@ var userArray = [];
 var round = 0;
 //sets status of start button
 var clicked = false;
+
+var timeoutID;
+
+var doneDisplaying = true;
 //set time to 1000
 var time = 1000;
 
-//User clicks the start button
+//User clicks the start button====================================================================
 $('#controls').click(function(){
 	if (clicked == false){
 		gameArray = [];
@@ -28,95 +32,109 @@ $('#controls').click(function(){
 })
 
 
-//Clears game & changes button back to start
+//Clears game & changes button back to start=======================================================
 function resetGame(){
 	userArray = [];
 	gameArray = [];
 	round = 0;
+	window.clearTimeout(timeoutID);
 	$('#controls').text('Start');
+	$('.boxes').addClass("boxes-opacity");
 	clicked = false;
 	}
 
-//Changes start button to Restart
+//Changes start button to Restart===============================================================
 function changeStartButton(){
 	$('#controls').text('Restart');
 }
 
-//User clicks the box
+//User clicks the box============================================================================
 $('.boxes').click(function(){
-	userArray.push(this.id)
-	compareArrays();
+	if (doneDisplaying == true && gameArray.length >= 0){
+		userArray.push(this.id)
+		compareArrays();
+	} else {
+		$('.boxes:active').css('');
+	}
 })
 
-//Makes the game faster
-$('#harder').click(function(){
-	time = time/1.5;
+//Makes the game faster=========================================================================
+var speed = 1;
+$('#level').click(function(){
+	speed ++;
+	if (speed <= 4){
+		time = time/1.3;
+		$('#level').text('Level ' + speed)
+	} else {
+		time = 1000;
+		$('#level').text('Level ' + speed)
+		return speed = 1
+	}
 })
 
 
-//Runs the game
+//Runs the game ================================================================================
 function runSequence(){
 	if (gameArray.length > 0) {
 		lightUpArray();
-		window.setTimeout(function(){
-			lightUpNew();
-		}, (round * time))
 	} else {
 		lightUpNew();
 	}
 }
-//Lights up the stored gameArray
+//Lights up the stored gameArray=================================================================
 function lightUpArray(){
+	doneDisplaying = false;
 	var i = 0;
 	var displayInterval = setInterval(function(){
+
+		if (i == gameArray.length){
+			doneDisplaying = true;
+			lightUpNew();
+			clearInterval(displayInterval);
+		}
+
+		window.setTimeout(function(){
+			 i++;
+		}, time * 0.501)
+
 		$("#" + gameArray[i]).removeClass("boxes-opacity");
 
 		window.setTimeout(function(){
-			$('#' + gameArray[i]).addClass("boxes-opacity");
+		$('#' + gameArray[i]).addClass("boxes-opacity");
 
 		}, time/2)
 
-		window.setTimeout(function(){
-			i++;
-		}, time * 0.6)
-		if (i == gameArray.length){
-			clearInterval(displayInterval);
-		}
 	}, time)
 }
 
 
-//Lights up the a random square
+//Lights up the a random square====================================================================
 function lightUpNew(){
+	if (doneDisplaying) {
 
-	var interval = setInterval(function(){
-		addRound();
-		
-		var random = Math.floor((Math.random() * 4) + 1);
+			addRound();
 
-		gameArray.push('box' + random);
+			var random = Math.floor((Math.random() * 4) + 1);
 
-		$("#box" + random).removeClass("boxes-opacity");
+			gameArray.push('box' + random);
 
-		window.setTimeout(function(){
-			$("#box" + random).addClass("boxes-opacity");
-		}, time/2)
+			$("#box" + random).removeClass("boxes-opacity");
 
+			window.setTimeout(function(){
+				$("#box" + random).addClass("boxes-opacity");
+			}, time/2)
 
-		if (round == gameArray.length){
-			clearInterval(interval);
-		}
-	 }, time);
+	}
 }
-//adds round to counter
+//adds round to counter============================================================================
 function addRound(){
 	round++;
 	$('#lcd-text').html('Round' + '<br>' + round);
 }
 
-//Compares Arrays via turning to string
+//Compares Arrays via turning to string=============================================================
 function compareArrays(){
-	if (gameArray.length == userArray.length){
+	if (gameArray.length == userArray.length && doneDisplaying == true){
 		if (gameArray.join('') == userArray.join('')) {
 			userArray = [];
 			runSequence();
@@ -131,7 +149,7 @@ function compareArrays(){
 	}
 }
 
-//Game Over function, melts off screen
+//Game Over function, melts off screen==============================================================
 function gameOver(){
 	$('.second-container').animate({
 		height: '+=6000px',
