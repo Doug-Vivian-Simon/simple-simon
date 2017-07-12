107489d5217ecafa4da409282eb15ad6d9e82f9c
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-11 19:21:41 -0500

    Added Comments to CSS and JS files, fixed upper margin of game

--- public/js/simple-simon.js
+++ public/js/simple-simon.js
@@ -1,12 +1,5 @@
-"use strict";
-
-//1 Make button funtion
-//2 display a pattern
-//3 check if user pattern matches display
-//3a if it does, move to next round
-//	1. Make note of current round,
-//	2. add one more to pattern
-//3b if no, start over
+(function(){
+	"use strict";
 
 //Computer's Array
 var gameArray = [];
@@ -29,7 +22,7 @@ $('.boxes').click(function(){
 	userArray.push(this.id)
 	compareArrays();
 })
-
+//Runs the game
 function runSequence(){
 	if (gameArray.length > 0) {
 		lightUpArray();
@@ -47,12 +40,11 @@ function runSequence(){
 function lightUpArray(){
 	var i = 0;
 	var displayInterval = setInterval(function(){
-		console.log("interval " + i);
 		$("#" + gameArray[i]).removeClass("boxes-opacity");
 
 		window.setTimeout(function(){
 			$('#' + gameArray[i]).addClass("boxes-opacity");
-			console.log("Timeout " + i);
+
 		}, 500)
 
 		window.setTimeout(function(){
@@ -99,11 +91,11 @@ function addRound(){
 function compareArrays(){
 	if (gameArray.length == userArray.length){
 		if (gameArray.join('') == userArray.join('')) {
-			console.log("you win");
+
 			userArray = [];
 			runSequence();
 		} else {
-			console.log('You lose');
+
 			gameOver();
 			userArray = [];
 			gameArray = [];
@@ -124,66 +116,4 @@ function gameOver(){
 		opacity: '+=6'
 	})
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function lightUpSquare() {
-
-// 	setTimeout(function(){
-		
-// 		// console.log(random)
-// 		//Using jQuery to brighten and darken colors via opacity
-// 		$('#box1').animate({
-// 			opacity: 1
-// 		}, 400).animate({
-// 			opacity: .4
-// 		}, 400);
-// 		gameArray.push("box1");
-
-// 	}, 750)
-// }
-
-
-
-
-
-
-		
-	// if (displayArray != []){
-	// 	displayArray.forEach(function(element){
-
-	// 		console.log("display array length: " + displayArray.length);
-	// 		console.log("counter:" + counter);
-
-	// 		// console.log(element);
-	// 		$('#' + element).animate({
-	// 			opacity: 1
-	// 		}, 400).animate({
-	// 			opacity: .4
-	// 		}, {
-	// 			duration: 400,
-	// 			complete: function(){
-	// 				counter ++;
-	// 				console.log(counter)
-	// 			}
-	// 		})
-	// 	})
-	// 	if (counter == (displayArray.length - 1)){
-	// 		lightUpSquare();
-	// 	}
-	// } else if (displayArray == []){
-	// 	lightUpSquare();
-	// }
+})();
